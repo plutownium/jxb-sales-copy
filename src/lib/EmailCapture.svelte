73a7a999ef555d283157801836ce78171f53bc69
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-02-12 20:05:13 -0800

    fully design squeeze page for desktop

--- src/lib/EmailCapture.svelte
+++ src/lib/EmailCapture.svelte
@@ -2,26 +2,61 @@
     console.log("foo!");
 </script>
 
-<div>
-
-    <h3>Email capture!!!</h3>
-</div>
-
-<div class="flex">
-    <div class="flex flex-col">
-        <div>
-            <h2>Get Your Free Beginner Programming Exercises Now!</h2>
+<div class="flex flex-col centering">
+    <h2>Give us your <span class="underline">best</span> email so we can send you our FREE report!</h2>
+    <div class="flex h-12 input-container">
+        <div class="m-2">
+            <input class="p-3" placeholder="Your Best Email" type="text" />
         </div>
-        <div>
-            <h3>Start Writing Code Today!</h3>
+        <div class="m-2">
+            <input class="p-3" placeholder="Your First Name" />
         </div>
     </div>
-    <div class="flex flex-col">
-        <div>
-            <input placeholder="Your Best Email" type="text"/>
-        </div>
-        <div>
-            <input placeholder="Your First Name"/>
-        </div>
-    </div>
+    <button class="cta-btn">Get My FREE Report</button>
 </div>
+
+<style>
+    .cta-btn {
+        color: white;
+        font-size: 18px;
+        margin: 12px;
+        height: 64px;
+        width: 218px;
+        background-color: #d44445;
+        border-radius: 12px;
+    }
+
+    .centering {
+        justify-content: center;
+        align-items: center;
+    }
+
+    .flex {
+        display: flex;
+    }
+
+    .flex-col {
+        flex-direction: column;
+    }
+
+    .h-12 {
+        height: 36px;
+    }
+
+    input {
+        border-radius: 5px;
+        border: 2px solid #bbb;
+    }
+
+    .input-container {
+        margin-bottom: 20px;
+    }
+
+    .m-2 {
+        margin: 6px;
+    }
+
+    .p-3 {
+        padding: 8px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,86 @@
 <script>
+    import BlueBox from "../lib/box/BlueBox.svelte";
+    import RedBox from "../lib/box/RedBox.svelte";
+    import WhiteBox from "../lib/box/WhiteBox.svelte";
+    import EmailCapture from "../lib/EmailCapture.svelte";
     import AttentionGrabber from "../lib/text/AttentionGrabber.svelte";
     import SqueezeHeadline from "../lib/text/SqueezeHeadline.svelte";
 </script>
 
-<main>
+<main class="">
     <!-- squeeze page  -->
-    <SqueezeHeadline />
-    <p>
-        Learn about the day of a junior developer, who only started his role <span class="underline">three weeks</span> prior.
-    </p>
-    <p>
-        Find out what the work day of a senior developer with only <span class="underline">four years</span> of experience
-        is like.
-    </p>
-    <p>And finally discover what it’s like to be a lead developer who manages twelve other programmers!</p>
+    <div class="always-margins always-top-margin">
+        <SqueezeHeadline />
+        <BlueBox>
+            <p>
+                Learn about the day of a junior developer, who only started his role <span class="underline"
+                    >three weeks</span
+                > prior.
+            </p>
+            <p>
+                Find out what the work day of a senior developer with only <span class="underline">four years</span> of experience
+                is like.
+            </p>
+            <p>
+                And finally discover what it’s like to be a <span class="underline">lead developer</span> who manages twelve
+                other programmers!
+            </p>
+        </BlueBox>
+        <WhiteBox>
+            <div class="h-24 center-stuff">
+                <AttentionGrabber bolded="PLEASE NOTE:"
+                    ><span class="">You <span class="underline">won't</span> find this on YouTube!</span
+                    ></AttentionGrabber
+                >
+            </div>
+        </WhiteBox>
+        <BlueBox>
+            <EmailCapture />
+        </BlueBox>
+    </div>
 </main>
 
 <style>
+    p {
+        font-size: 20px;
+    }
+
+    .always-top-margin {
+        margin-top: 20px;
+    }
+
+    .always-margins {
+        margin: 0px 100px;
+    }
+
+    @media screen and (max-width: 1100px) {
+        .always-margins {
+            margin: 0 40px;
+        }
+    }
+
+    @media screen and (max-width: 800px) {
+        .always-margins {
+            margin: 0 30px;
+        }
+    }
+
+    @media screen and (max-width: 500px) {
+        .always-margins {
+            margin: 0 20px;
+        }
+    }
+
+    .h-24 {
+        height: 48px;
+    }
+
+    .center-stuff {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
     .underline {
         text-decoration: underline;
     }
