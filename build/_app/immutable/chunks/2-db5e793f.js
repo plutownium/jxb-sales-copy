37a90d23f45a0af269570b9b2e99a32e93b4a511
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-02-18 14:18:13 -0800

    build

---
+++ build/_app/immutable/chunks/2-db5e793f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-4db3809a.js";export{t as component};
---
+++ build/_app/immutable/chunks/3-60e536c1.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/change-your-life/_page.svelte-d8c0ee84.js";export{t as component};
